12af24010391129499841771a83328fe4ed90cb9
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2021-04-12 12:39:47 -0700

    Start folder for doc with info on rec data

---
+++ doc/lingcod_21f_rec_00.R
@@ -0,0 +1,6 @@
+Data on catches for the recreational fleets were dowloaded from
+Recreational Fisheries Information Network ([RecFIN][]) and
+provided by state representatives.
+**todo: check that the following statement is true and if also true for commercial move to generic catch section.**
+The units of all catches are in mt and include both dead, landed fish and dead, discarded fish.
+
