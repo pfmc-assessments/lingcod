897be94b35f7ad13ca3cd5aaa5cba9a95c8c5b26
kellijohnson-NOAA <kelli.johnson@nooa.gov>
2021-05-20 15:17:53 -0700

    create a csv file to send to TT with diff in rec comp data

--- unfit/lingcod_rec_wa.R
+++ unfit/lingcod_rec_wa.R
@@ -2,22 +2,22 @@
 #'
 #' What file are we supposed to use?
 #'
-file_new <- "SD501--2001---2020_rec_bio_lingcod_pulled_4_19_21.csv"
-file_old <- "SD506--1984---2020_rec_ageing_lincod_pulled_4_19_21.csv"
+file_length <- "SD501--2001---2020_rec_bio_lingcod_pulled_4_19_21.csv"
+file_age <- "SD506--1984---2020_rec_ageing_lincod_pulled_4_19_21.csv"
 file_wa <- "Lingcod Biodata as of 3_29_2021(More Ages Coming Daily).xlsx"
 
-rec_comp_new <- utils::read.csv(
-  file = file.path("data-raw", file_new)
+recfin_comp_length <- utils::read.csv(
+  file = file.path("data-raw", file_length)
 )  %>%
   dplyr::mutate(
-    source = "recFINnew",
+    source = "recFINl",
     year = RECFIN_YEAR
   )
-rec_comp_old <- utils::read.csv(
-  file = file.path("data-raw", file_old)
+recfin_comp_age <- utils::read.csv(
+  file = file.path("data-raw", file_age)
 )  %>%
   dplyr::mutate(
-    source = "recFINold",
+    source = "recFINa",
     year = SAMPLE_YEAR,
     age = as.integer(USE_THIS_AGE)
   )
@@ -32,32 +32,32 @@ rec_comp_wa <- readxl::read_excel(
     RECFIN_LENGTH_MM = fish_length_cm * 10
   )
 
-dplyr::full_join(
+missinga <- dplyr::full_join(
   by = c(
     "year",
     "age",
     "RECFIN_LENGTH_MM",
     "source"
   ),
-  x = rec_comp_old %>% dplyr::filter(SAMPLING_AGENCY_NAME == "WDFW"),
+  x = recfin_comp_age %>% dplyr::filter(SAMPLING_AGENCY_NAME == "WDFW"),
   y = rec_comp_wa
 ) %>%
   dplyr::filter(!is.na(age)) %>%
   dplyr::count(year, source) %>%
   tidyr::spread(key = "source", val = "n") %>%
   dplyr::mutate(diffa =
-    ifelse(is.na(recFINold), 0, recFINold) -
+    ifelse(is.na(recFINa), 0, recFINa) -
     ifelse(is.na(WDFW), 0, WDFW)
   ) %>%
   dplyr::filter(diffa != 0)
 
-dplyr::full_join(
+missingl <- dplyr::full_join(
   by = c(
     "year",
     "RECFIN_LENGTH_MM",
     "source"
   ),
-  x = rec_comp_new %>% dplyr::filter(STATE_NAME == "WASHINGTON"),
+  x = recfin_comp_length %>% dplyr::filter(STATE_NAME == "WASHINGTON"),
   y = rec_comp_wa
 ) %>%
   dplyr::mutate(bin = cut(
@@ -65,10 +65,21 @@ dplyr::full_join(
     seq(0, max(RECFIN_LENGTH_MM, na.rm = TRUE) + 100, 100),
     right = FALSE
   )) %>%
-  dplyr::count(bin, source) %>%
+  dplyr::count(year, source) %>%
   tidyr::spread(key = "source", val = "n") %>%
   dplyr::mutate(diffl =
-    ifelse(is.na(recFINnew), 0, recFINnew) -
+    ifelse(is.na(recFINl), 0, recFINl) -
     ifelse(is.na(WDFW), 0, WDFW)
   ) %>%
   dplyr::filter(diffl != 0)
+
+utils::write.csv(
+  x = dplyr::full_join(
+    by = "year",
+    x = missinga,
+    y = missingl,
+    suffix = c(".a", ".l")
+  ),
+  file = file.path("unfit", "lingcod_rec_wa_diff.csv"),
+  row.names = FALSE
+)
