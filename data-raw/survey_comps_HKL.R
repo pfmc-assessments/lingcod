b01f1439a8fd70dd2e0e44bd4ef835f0a4bd3e51
kellijohnson-NOAA <kelli.johnson@nooa.gov>
2021-05-28 11:00:34 -0700

    modify survey_comps_HKL to work with data-raw instead of data

--- data-raw/survey_comps_HKL.R
+++ data-raw/survey_comps_HKL.R
@@ -14,8 +14,6 @@
 #1. Need to define length bins (currently set to min/max of each dataset with bin size of 2) DONE
 #2. Will need to specify survey timing (month 7), sex (currently 3 or 0), fleet number (8), partition (0), etc....
 
-library(nwfscSurvey)
-
 ##------------------------------------Scripts----------------------------------------##
 
 #These were saved as .rda within the package in lingcod_LW_LA.R and replace having to read in again
@@ -33,8 +31,9 @@ hnl_ages = bio.HKLage.Lam
 # hnl_ages = read.csv(file.path("data-raw", "H&L_Lingcod_ages.csv"), header = TRUE)
 
 #Create subfolder in length and age directory
-if(!dir.exists(file.path("data", "lenComps", "HooknLine"))) dir.create(file.path("data", "lenComps", "HooknLine"))
-if(!dir.exists(file.path("data", "ageComps", "HooknLine"))) dir.create(file.path("data", "ageComps", "HooknLine"))
+dir <- file.path("data-raw", c(sprintf("%sComps", c("len", "age")), "ageCAAL"), "HooknLine")
+ignore <- mapply(dir.create, dir, MoreArgs = list(showWarnings = FALSE, recursive = TRUE))
+stopifnot(file.exists(dir))
 
 
 ############################################################################################
@@ -49,33 +48,35 @@ hnl$Sex = hnl$sex
 hnl$Year = hnl$year
 
 hnl$Trawl_id = 1:nrow(hnl)
-GetN.fn(dir = file.path("data", "lenComps", "HooknLine"), dat = hnl, type = "length", species = 'others')
-n = read.csv(file.path("data", "lenComps", "HooknLine", "forSS", "length_SampleSize.csv"))
+nwfscSurvey::GetN.fn(dir = dir[1], dat = hnl, type = "length", species = 'others')
+n = read.csv(file.path(dir[1], "forSS", "length_SampleSize.csv"))
 n = n[,c('Year', 'All_Fish', 'Sexed_Fish', 'Unsexed_Fish')]
-write.csv(n, file = file.path("data", "lenComps", "HooknLine", "hkl_samples.csv"), row.names = FALSE)
+write.csv(n, file = file.path(dir[1], "hkl_samples.csv"), row.names = FALSE)
 #Remove forSS file
-unlink(file.path("data", "lenComps", "HooknLine","forSS"), recursive=TRUE)
+unlink(file.path(dir[1],"forSS"), recursive = TRUE)
 
 #Generate Comps
-lfs = UnexpandedLFs.fn(dir = file.path("data", "lenComps"), #puts into "forSS" folder in this location
+lfs = nwfscSurvey::UnexpandedLFs.fn(dir = dirname(dir[1]), #puts into "forSS" folder in this location
                        datL = hnl, lgthBins = info_bins[["length"]], printfolder = "HooknLine",
-                       sex = 3,  partition = 0, fleet = 8, month = 7)
-file.rename(from = file.path("data", "lenComps", "HooknLine", paste0("Survey_notExpanded_Length_comp_Sex_3_bin=", min(info_bins[["length"]]), "-", max(info_bins[["length"]]), ".csv")), 
-            to = file.path("data", "lenComps", "HooknLine", paste0("south_HNL_notExpanded_Length_comp_Sex_3_bin=", min(info_bins[["length"]]), "-", max(info_bins[["length"]]), ".csv"))) 
-file.rename(from = file.path("data", "lenComps", "HooknLine", paste0("Survey_notExpanded_Length_comp_Sex_0_bin=", min(info_bins[["length"]]), "-", max(info_bins[["length"]]), ".csv")), 
-            to = file.path("data", "lenComps", "HooknLine", paste0("south_HNL_notExpanded_Length_comp_Sex_0_bin=", min(info_bins[["length"]]), "-", max(info_bins[["length"]]), ".csv"))) 
+                       sex = 3,  partition = 0, fleet = get_fleet("Hook")$num, month = 7)
+file.rename(from = file.path(dir[1], paste0("Survey_notExpanded_Length_comp_Sex_3_bin=", min(info_bins[["length"]]), "-", max(info_bins[["length"]]), ".csv")), 
+            to = file.path(dir[1], paste0("south_HNL_notExpanded_Length_comp_Sex_3_bin=", min(info_bins[["length"]]), "-", max(info_bins[["length"]]), ".csv"))) 
+file.rename(from = file.path(dir[1], paste0("Survey_notExpanded_Length_comp_Sex_0_bin=", min(info_bins[["length"]]), "-", max(info_bins[["length"]]), ".csv")), 
+            to = file.path(dir[1], paste0("south_HNL_notExpanded_Length_comp_Sex_0_bin=", min(info_bins[["length"]]), "-", max(info_bins[["length"]]), ".csv"))) 
 
 #Visualize
-PlotFreqData.fn(dir = file.path("data", "lenComps", "HooknLine"), 
+nwfscSurvey::PlotFreqData.fn(dir = dir[1],
                 dat = lfs$comps, ylim=c(0, max(info_bins[["length"]])+4),
                 main = "HNL lengths Male-Female", yaxs="i", ylab="Length (cm)", dopng = TRUE)
-PlotFreqData.fn(dir = file.path("data", "lenComps", "HooknLine"), 
+nwfscSurvey::PlotFreqData.fn(dir = dir[1],
                 dat = lfs$comps_u, ylim=c(0, max(info_bins[["length"]])+4),
                 main = "HNL lengths Unsexed", yaxs="i", ylab="Length (cm)", dopng = TRUE)
-PlotSexRatio.fn(dir = file.path("data", "lenComps", "HooknLine"),
+nwfscSurvey::PlotSexRatio.fn(dir = dir[1],
                 dat = hnl, ylim = c(-0.1, 1.1), main = "HNL Sex Ratio", yaxs="i", dopng = TRUE)
 
-age_representativeness_plot(hnl)
+suppressWarnings(
+  age_representativeness_plot(hnl, file = file.path("figures", "HooknLine_agerepresentativeness.png"))
+)
 
 #Save as .rdas. Combined for sex3 (first element) and unsexed
 lenCompS_HKL = lfs
@@ -90,22 +91,22 @@ hnl_ages$Sex = hnl_ages$sex
 hnl_ages$Year = hnl_ages$year
 
 hnl_ages$Trawl_id = 1:nrow(hnl_ages)
-GetN.fn(dir = file.path("data", "ageComps", "HooknLine"), dat = hnl_ages, type = "age", species = 'others')
-n = read.csv(file.path("data", "ageComps", "HooknLine", "forSS", "age_SampleSize.csv"))
+nwfscSurvey::GetN.fn(dir = dir[2], dat = hnl_ages, type = "age", species = 'others')
+n = read.csv(file.path(dir[2], "forSS", "age_SampleSize.csv"))
 n = n[,c('Year', 'All_Fish', 'Sexed_Fish', 'Unsexed_Fish')]
-write.csv(n, file = file.path("data", "ageComps", "HooknLine", "hkl_age_samples.csv"), row.names = FALSE)
+write.csv(n, file = file.path(dir[2], "hkl_age_samples.csv"), row.names = FALSE)
 #Remove forSS file
-unlink(file.path("data", "ageComps", "HooknLine","forSS"), recursive=TRUE)
+unlink(file.path(dir[2],"forSS"), recursive=TRUE)
 
-afs = UnexpandedAFs.fn(dir = file.path("data", "ageComps"),  #Somehow stills prints to "forSS"
+afs = nwfscSurvey::UnexpandedAFs.fn(dir = dirname(dir[2]),  #Somehow stills prints to "forSS"
                        datA = hnl_ages, ageBins = info_bins[["age"]], printfolder = "HooknLine",
-                       sex = 3, partition = 0, fleet = 8, month = 7, ageErr = 1)
-file.rename(from = file.path("data", "ageComps", "forSS", "Survey_notExpanded_Age_comp_Sex_3_bin=0-20.csv"), 
-            to= file.path("data", "ageComps", "HooknLine", "south_Survey_Sex3_Bins_0_20_AgeComps.csv")) 
-if(dir.exists(file.path("data", "ageComps","forSS"))) unlink(file.path("data", "ageComps", "forSS"),recursive = TRUE) #remove forSS file
+                       sex = 3, partition = 0, fleet = get_fleet("Hook")$num, month = 7, ageErr = 1)
+file.rename(from = file.path(dirname(dir[2]), "forSS", "Survey_notExpanded_Age_comp_Sex_3_bin=0-20.csv"), 
+            to= file.path(dir[2], "south_Survey_Sex3_Bins_0_20_AgeComps.csv")) 
+if(dir.exists(file.path(dirname(dir[2]),"forSS"))) unlink(file.path("data", "ageComps", "forSS"),recursive = TRUE) #remove forSS file
 
-PlotFreqData.fn(dir = file.path(getwd(), "data", "ageComps", "HooknLine"), dat = afs$comps, ylim=c(0, max(info_bins[["age"]]) + 1), inch = 0.10, main = "Hook and Line", yaxs="i", ylab="Age", dopng = TRUE)
-PlotSexRatio.fn(dir = file.path(getwd(), "data", "ageComps", "HooknLine"), dat = hnl_ages, data.type = "age", dopng = TRUE, main = "Hook and Line")
+nwfscSurvey::PlotFreqData.fn(dir = dir[2], dat = afs$comps, ylim=c(0, max(info_bins[["age"]]) + 1), inch = 0.10, main = "Hook and Line", yaxs="i", ylab="Age", dopng = TRUE)
+nwfscSurvey::PlotSexRatio.fn(dir = dir[2], dat = hnl_ages, data.type = "age", dopng = TRUE, main = "Hook and Line")
 
 #Save as .rdas. Just sex3 (there are no unsexed ages)
 ageCompS_HKL = afs
@@ -117,9 +118,17 @@ usethis::use_data(ageCompS_HKL, overwrite = TRUE)
 hnl_ages$Len_Bin_FL = 2*floor(hnl_ages$Length_cm/2)
 hnl_ages$Ages = hnl_ages$Age
 
-ageCAAL_S_HKL = create_caal_nonsurvey(Data = hnl_ages, agebin = arange, lenbin = lrange, wd = "data/ageCAAL/HooknLine", 
-                                      append = "south_Survey", seas = 7, fleet = 8, partition = 0, ageEr = 1)
+ageCAAL_S_HKL = create_caal_nonsurvey(Data = hnl_ages, agebin = range(info_bins[["age"]]), lenbin = range(info_bins[["length"]]), wd = dir[3], 
+                                      append = "south_Survey", seas = 7, fleet = get_fleet("Hook")$num, partition = 0, ageEr = 1)
 
 usethis::use_data(ageCAAL_S_HKL, overwrite = TRUE)
 
-
+ignore <- file.copy(
+  recursive = TRUE,
+  unlist(mapply(
+    FUN = dir,
+    dir,
+    MoreArgs = list(recursive = TRUE, full.names = TRUE, pattern = "png")
+  )),
+  "figures"
+)
