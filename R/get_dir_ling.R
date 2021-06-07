f98037bcc51aba7bd3d8f5d490d8b6075619d44b
kellijohnson-NOAA <kelli.johnson@nooa.gov>
2021-06-07 15:40:04 -0700

    goofed up previous commit b/c I didn't remove the code from the original file.
    This commit fixes the previous.
    @iantaylor-noaa I split out the section of code I wanted :)

--- R/get_dir_ling.R
+++ R/get_dir_ling.R
@@ -60,33 +60,3 @@ get_dir_ling <- function(area = NULL,
   }
   return(dir)
 }
-
-#' Get a markdown table from within a section in a .md file
-#' @param file A file path to a .md file.
-#' @param section A text string of the section in the markdown file
-#' that holds the table you want. This section should only contain the table,
-#' no other text.
-get_mdtable <- function(file, section) {
-
-  stopifnot(file.exists(file))
-  stopifnot(is.character(section))
-
-  strings <- readLines(file)
-  start <- grep(section, strings)
-  end <- grep("^#\\s+", strings)
-  end <- ifelse(
-    length(end[which(grep("^#\\s+", strings) > start)]) == 0,
-    length(strings),
-    end[which(grep("^#\\s+", strings) > start)][1]
-  )
-  models <- readr::read_delim(
-    file = strings[start:end],
-    skip = 1,
-    delim = "|",
-    trim_ws = TRUE,
-    col_types = readr::cols()
-  )
-  out <- models[!apply(models, 1, function(x) all(grepl("^-+$", x))), ]
-
-  return(out)
-}
