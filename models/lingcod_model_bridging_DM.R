3d7517af67bd51c09fb29bc16b21fcc5fa908cc4
andi-stephens-NOAA <Andi.Stephens@noaa.gov>
2021-06-10 11:02:25 -0700

    Added commands to create new models and run the DM tuning.

--- models/lingcod_model_bridging_DM.R
+++ models/lingcod_model_bridging_DM.R
@@ -1,5 +1,34 @@
 # fill in stuff from Andi here on setting up Dirichlet-Multinomial likelihoods
 
+copy_SS_inputs(dir.old="2019.n.003.001_update_catch_2019_structure", 
+               dir.new="2019.n.004.001_DM")
+copy_SS_inputs(dir.old="2019.s.003.001_update_catch_2019_structure", 
+               dir.new="2019.s.004.001_DM")
+
+copy_SS_inputs(dir.old="2021.s.002.001_new_fleets", 
+               dir.new="2021.s.003.001_DM")
+copy_SS_inputs(dir.old="2021.n.002.001_new_fleets", 
+               dir.new="2021.n.003.001_DM")
+
+# At this point, I used a shell to run SS to get the outputs.  Could just have
+# copied the whole contents of the model folders to new names and skipped this
+# step.
+
+setwd("2019.s.004.001_DM")
+mod = r4ss::SS_output(".")
+r4ss::SS_tune_comps(mod, option = "DM", niters_tuning = 1)
+
+setwd("../2019.n.004.001_DM")
+mod = r4ss::SS_output(".")
+r4ss::SS_tune_comps(mod, option = "DM", niters_tuning = 1)
+
+setwd("../2021.s.003.001_DM")
+mod = r4ss::SS_output(".")
+r4ss::SS_tune_comps(mod, option = "DM", niters_tuning = 1, extras = "-nohess")
+                      
+setwd("2021.n.003.001_DM")
+mod = r4ss::SS_output(".")
+r4ss::SS_tune_comps(mod, option = "DM", niters_tuning = 1, extras = "-nohess")
 
 # load model results (creates objects in the workspace)
 
