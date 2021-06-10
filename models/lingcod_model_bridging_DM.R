4056f9d8f86c3daa0d61e815156502e1700722ca
andi-stephens-NOAA <Andi.Stephens@noaa.gov>
2021-06-10 16:33:35 -0700

    Modification to lingcod_model_bridging_DM.R, recreate figures.

--- models/lingcod_model_bridging_DM.R
+++ models/lingcod_model_bridging_DM.R
@@ -33,15 +33,17 @@ r4ss::SS_tune_comps(mod, option = "DM", niters_tuning = 1, extras = "-nohess")
 # load model results (creates objects in the workspace)
 
 # get 2019 models with 3.30.16.02 executable
-get_mod(area = 'n', num = 2, yr = 2019)
-get_mod(area = 's', num = 2, sens = 2, yr = 2019)
+get_mod(area = 'n', num = 3, yr = 2019)
+get_mod(area = 's', num = 3, yr = 2019)
 # get 2019 models with DM weighting for comps
 get_mod(area = 'n', num = 4, yr = 2019)
 get_mod(area = 's', num = 4, yr = 2019)
 
+get_mod(area="s", num=2, sens=2, yr=2019)
+
 # make plots comparing before and after DM likelihood
-plot_twopanel_comparison(list(mod.2019.n.002.001, mod.2019.n.004.001))
-plot_twopanel_comparison(list(mod.2019.s.002.002, mod.2019.s.004.001))
+plot_twopanel_comparison(list(mod.2019.n.003.001, mod.2019.n.004.001))
+plot_twopanel_comparison(list(mod.2019.s.003.001, mod.2019.s.004.001))
 
 
 # get 2021 models (main difference is fleet structure)
@@ -52,11 +54,11 @@ get_mod(area = 'n', num = 3)
 get_mod(area = 's', num = 3)
 
 # make plots comparing before and after DM likelihood
-plot_twopanel_comparison(list(mod.2019.n.002.001,
+plot_twopanel_comparison(list(mod.2019.n.003.001,
                               mod.2019.n.004.001,
                               mod.2021.n.002.001,
                               mod.2021.n.003.001))
-plot_twopanel_comparison(list(mod.2021.s.002.001,
+plot_twopanel_comparison(list(mod.2021.s.003.001,
                               mod.2021.s.003.001,
                               mod.2019.s.002.002,
                               mod.2019.s.004.001))
