e05728d7707380b0d2c31390d58bfb6991eed292
kellijohnson-NOAA <kelli.johnson@nooa.gov>
2021-06-16 07:56:46 -0700

    feat(ggsave2): augment ggsave with caption, alttext, label

---
+++ R/ggsave2.R
@@ -0,0 +1,27 @@
+#' Save a [ggplot2::ggplot] figure with caption and alt text
+#'
+#' Save a [ggplot2::ggplot] figure and a csv file with additional
+#' information, where the figure and csv are saved with the same
+#' name just different extensions.
+#'
+#' @param ... Parameters passed to [ggplot2::ggsave].
+#' @param caption A character string providing the caption.
+#' @param alttext A character string providing the alternative text.
+#' @param label A character string providing the LaTeX label.
+#'
+#' @author Kelli F. Johnson
+#' @export
+#'
+ggsave2 <- function(..., caption, alttext, label) {
+
+  ggplot2::ggsave(...)
+  dots <- list(...)
+  outdf <- data.frame(
+    caption = caption,
+    alt_caption = alttext,
+    label = label,
+    filein = dots[["filename"]]
+  )
+  utils::write.csv(outdf, gsub("\\.[pngjpg]{3}", ".csv", dots[["filename"]]))
+  return()
+}
