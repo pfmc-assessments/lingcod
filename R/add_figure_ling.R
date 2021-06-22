b136b55d9922be93ded1d3a3cd0fb0bec51de2e9
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2021-06-22 14:36:16 -0700

    start simplifying figures

---
+++ R/add_figure_ling.R
@@ -0,0 +1,29 @@
+#' Add a data frame of figures.
+#'
+#' A wrapper for [sa4ss::add_figure].
+#' @param csv The csv file with all of the information per figure in rows.
+#' @param grep The labels you want.
+#' @param caption A vector of alternative captions.
+#' Only needs to be supplied if you want to change the values in `csv`.
+#' @param alt_caption A vector of alternative alt text.
+#' Only needs to be supplied if you want to change the values in `csv`.
+#' @author Kelli F. Johnson
+#' @examples
+#' \dontrun{
+#' add_figure_ling(csv=figurecsv, "data_plot")
+#' }
+add_figure_ling <- function(csv, grep = ".+", caption, alt_caption) {
+  info <- csv %>%
+    dplyr::filter(grepl(grep, label)) %>%
+    dplyr::rename_with(
+        ~ dplyr::case_when(
+            . == "loc" ~ "filein",
+            . == "altcaption" ~ "alt_caption",
+            TRUE ~ .
+        )
+    ) %>%
+    dplyr::select(filein, caption, label, alt_caption)
+  if (!missing(caption)) info[, "caption"] <- caption
+  if (!missing(alt_caption)) info[, "alt_caption"] <- alt_caption
+  ignore <- apply(info, 1, function(x) do.call(sa4ss::add_figure, as.list(x)))
+}
