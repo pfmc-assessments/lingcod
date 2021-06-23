4e873acfbf32f243aec161cb90a2f776d00263ea
Ian Taylor <ian.taylor@noaa.gov>
2021-06-23 10:44:20 -0700

    add custom index plots to be created by lingcod_builddoc.R #74

--- R/make_r4ss_plots_ling.R
+++ R/make_r4ss_plots_ling.R
@@ -5,6 +5,8 @@
 #'
 #' @param mod List created by [get_mod()] or `r4ss::SS_output()`
 #' @param plot Vector of plot groups passed to `r4ss::SS_plots()`
+#' as well as cutom plots with numbers starting at 31
+#' (see code for which plots match which numbers).
 #' @template verbose
 #' @param dots additional arguments passed to `r4ss::SS_plots()`
 #' @author Ian G. Taylor
@@ -12,7 +14,8 @@
 #' @seealso [get_mod()]
 #' 
 
-make_r4ss_plots_ling <- function(mod, plot = 1:26, verbose = TRUE, ...) {
+make_r4ss_plots_ling <- function(mod, plot = c(1:26, 31:50),
+                                 verbose = TRUE, ...) {
   area <- mod$area # extra element added by get_mod()
   # get all info on fleets
   fleets <- get_fleet()
@@ -20,25 +23,46 @@ make_r4ss_plots_ling <- function(mod, plot = 1:26, verbose = TRUE, ...) {
   # r4ss functionality for `fleetcolors` is incomplete and inconsistent,
   # so use depends on the function
   showfleets <- fleets[fleets[[paste0("used_2021.", area)]], ]
-  
-  # make default plots for most things
-  r4ss::SS_plots(mod,
-           plot = intersect(plot, c(1:23, 25:26)),
-           fleets = showfleets$num,
-           fleetnames = fleets$label_long,
-           fleetcols = fleets[[paste0("col.", area)]],
-           comp.yupper = 0.15,
-           html = FALSE, # don't open HTML view yet
-           verbose = verbose, ...)
-  # make data plot with wider margin and taller to fit all fleet names
-  r4ss::SS_plots(mod,
-           plot = intersect(plot, 24),
-           fleets = showfleets$num,
-           fleetnames = fleets$label_long,
-           fleetcols = showfleets[[paste0("col.", area)]],
-           pheight_tall = 8,
-           SSplotDatMargin = 12, # add extra margin for data plot
-           html = TRUE, # now open HTML view
-           verbose = verbose, ...)
-  
+
+  if (any(1:26 %in% plot)) {
+    # make default plots for most things
+    r4ss::SS_plots(mod,
+                   plot = intersect(plot, c(1:23, 25:26)),
+                   fleets = showfleets$num,
+                   fleetnames = fleets$label_long,
+                   fleetcols = fleets[[paste0("col.", area)]],
+                   comp.yupper = 0.15,
+                   html = FALSE, # don't open HTML view yet
+                   verbose = verbose, ...)
+    # make data plot with wider margin and taller to fit all fleet names
+    r4ss::SS_plots(mod,
+                   plot = intersect(plot, 24),
+                   fleets = showfleets$num,
+                   fleetnames = fleets$label_long,
+                   fleetcols = showfleets[[paste0("col.", area)]],
+                   pheight_tall = 8,
+                   SSplotDatMargin = 12, # add extra margin for data plot
+                   html = TRUE, # now open HTML view
+                   verbose = verbose, ...)
+  }
+
+  # custom plots not created by r4ss
+  if (any(31:50 %in% plot)) {
+    dir <- file.path(mod$inputs$dir, "custom_plots")
+    if (!dir.exists(dir)) {
+      dir.create(dir)
+    }
+  }
+
+  # index data and fits
+  if (31 %in% plot){
+    plot_indices(mod, fit = FALSE)
+  } 
+  if (32 %in% plot){
+    plot_indices(mod, fit = TRUE)
+  }  
+  if (33 %in% plot){
+    plot_indices(mod, fit = TRUE, log = TRUE)
+  } 
+
 }
--- R/plot_indices.R
+++ R/plot_indices.R
@@ -6,7 +6,9 @@
 #' @param mod A model object created by [get_mod()]. Could also be
 #' created by `r4ss::SS_output()` but needs to have an element "area"
 #' added to the list.
-#'
+#' @param png write to png file or existing plot device
+#' @param fit include fit or just data only
+#' @param log plot on a log scale
 #' @export
 #' @author Ian G. Taylor
 #' @return A figure showing the fit to all indices.
@@ -15,20 +17,74 @@
 #'   plot_indices(mod.2021.n.008.004)
 #' }
 
-plot_indices <- function(mod) {
+plot_indices <- function(mod, png = TRUE, fit = TRUE, log = FALSE) {
+
+  if (png) {
+    filename <- "index_fits_all_fleets.png"
+    subplot = 2
+
+    # make caption
+    caption <- paste0("Index fits for all fleets.",
+                      "Lines indicate 95\\% uncertainty interval around index values ",
+                      "based on the model assumption of lognormal error. ",
+                      "Thicker lines (if present) indicate input uncertainty ",
+                      "before addition of an estimated additional uncertainty parameter ",
+                      "which is added to the standard error of all years within an index.")
+
+    if (log & !fit) {
+      stop("Function doesn't support log-scale plot without fits")
+    }
+    if (log) {
+      subplot = 5
+      filename <- gsub("index", "index_log", filename)
+      caption <- gsub("fits", "fits on a log scale", caption)
+    }
+    if (!fit) {
+      subplot = 1
+      filename <- gsub("fits", "data", filename)
+      caption <- paste0("Index observations for all fleets with 95\\% uncertainty ",
+                        "intervals as input to the model before any estimation of ",
+                        "additional uncertainty parameters.")
+    }
+
+    filepath <- file.path(mod$inputs$dir, "custom_plots", filename)
+    
+    alt_caption <- strsplit(caption, split = ".", fixed = TRUE)[[1]][1]
+    write.csv(x = data.frame(caption = caption,
+                             alt_caption = alt_caption,
+                             label = gsub(".png", "", filename),
+                             filein = file.path("..", filepath)),
+              file = gsub("png", "csv", filepath),
+              row.names = FALSE)
+
+    # open PNG file
+    png(filepath, width = 6.5, height = 4, units = "in", res = 300, pointsize = 10)
+  }
+
   if (mod$area == "n") {
-    par(mfrow = c(2,4))
+    par(mfrow = c(2,4), mar = c(3,1,1,1), oma = c(2,2,0,0))
   }
   if (mod$area == "s") {
-    par(mfrow = c(2,3))
+    par(mfrow = c(2,3), mar = c(3,1,1,1), oma = c(2,2,0,0))
   }
+
+  # get fleets with indices
   fleets <- unique(mod$cpue$Fleet)
+
+  # make a panel for each one
   for(f in fleets) {
     r4ss::SSplotIndices(mod,
                         fleets = f,
-                        subplots = 2,
+                        subplots = subplot,
                         col3 = get_fleet(f, col = paste0("col.", mod$area)),
                         mainTitle = FALSE)
     title(main = get_fleet(f, col = "label_short"))
+  }
+  mtext("Year", side = 1, line = 1, outer = TRUE)
+  mtext(ifelse(log, "Log-scale index", "Index"), side = 1, line = 1, outer = TRUE)
+
+  # close png file
+  if (png) {
+    dev.off()
   }
 }
