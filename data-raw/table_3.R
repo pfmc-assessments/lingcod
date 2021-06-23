ebd50c22ac39fb949f6d1b30238ba92607c2db00
Brian Langseth <brian.langseth@noaa.gov>
2021-06-23 09:54:30 -0700

    Add table of age samples

--- data-raw/table_3.R
+++ data-raw/table_3.R
@@ -64,9 +64,13 @@ table3_deb = function(comp){
 }
 
 
-table3_exp = function(comp, region){
+table3_exp = function(comp, region, type){
   
   #Region must be either "north" or "south" to obtain sample sizes
+  #Type must be either "length" or "age" to obtain sample sizes
+  
+  if(type == "length") abbr_type = "len"
+  if(type == "age") abbr_type = type
   
   #Expanded comps. Comps with sexed fish.
 
@@ -80,7 +84,7 @@ table3_exp = function(comp, region){
   sex$Fleet <- get_fleet(sex$fleet)$label_short
   
   samps = read.csv(
-    file.path(getwd(),"data-raw","lenComps",sex$Fleet[1],paste0(region,"_length_SampleSize.csv")),
+    file.path(getwd(),"data-raw",paste0(abbr_type,"Comps"),sex$Fleet[1],paste0(region,"_",type,"_SampleSize.csv")),
     header = TRUE)
   
   sex$Nfish <- samps$All_Fish 
@@ -146,6 +150,30 @@ table3_exp_comm = function(comp){
   return(rbind(sex,unsex))
 }
 
+#Age comp function to get marginal from CAAL
+table4_fromCAAL = function(comp){
+  
+  #female first
+  fem <- comp$female[,c("Year", "Fleet", "Gender", "nSamps")]
+  
+  #male next
+  mal <- comp$male[,c("Year", "Fleet", "Gender", "nSamps")]
+  
+  comb = data.frame("year" = unique(c(fem$Year, mal$Year)))
+  comb$Nmale <- (dplyr::group_by(mal, Year) %>% dplyr::summarise(sum = sum(nSamps)))$sum
+  comb$Nfemale <- (dplyr::group_by(fem, Year) %>% dplyr::summarise(sum = sum(nSamps)))$sum
+  comb$sex = 3
+  comb$Gender <- "Sexed"
+  comb$Units <- "Nfish"
+  comb$Nfish <- comb$Nmale + comb$Nfemale
+  comb$Nsamp <- ""
+  comb$fleet <- unique(c(fem$Fleet,mal$Fleet))
+  comb$Fleet <- get_fleet(unique(comb$fleet))$label_short
+  comb$Ntows <- ""
+  
+  return(comb)
+}
+
 ##################################End of functions##################################
 
 ####################
@@ -157,8 +185,8 @@ t3_or_rec <- table3_noexp(lenCompN_OR_Rec)
 t3_wa_rec <- table3_noexp(lenCompN_WA_Rec)
 t3_lam <- table3_noexp(lenCompN_LamThesis)
 
-t3_wcgbts <- table3_exp(lenCompN_sex3_WCGBTS, region = "north")
-t3_tri <- table3_exp(lenCompN_sex3_Triennial, region = "north")
+t3_wcgbts <- table3_exp(lenCompN_sex3_WCGBTS, region = "north", type = "length")
+t3_tri <- table3_exp(lenCompN_sex3_Triennial, region = "north", type = "length")
 
 t3_discards <-table3_exp_discard(lenCompN_comm_discards)
 t3_com <-table3_exp_comm(lenCompN_comm)
@@ -195,8 +223,8 @@ t3_lam <- table3_noexp(lenCompS_LamThesis)
 t3_hkl <- table3_noexp(lenCompS_HKL)
 t3_deb <- table3_deb(lenCompS_debHist)
   
-t3_wcgbts <- table3_exp(lenCompS_sex3_WCGBTS, region = "south")
-t3_tri <- table3_exp(lenCompS_sex3_Triennial, region = "south")
+t3_wcgbts <- table3_exp(lenCompS_sex3_WCGBTS, region = "south", type = "length")
+t3_tri <- table3_exp(lenCompS_sex3_Triennial, region = "south", type = "length")
 
 t3_discards <-table3_exp_discard(lenCompS_comm_discards)
 t3_com <-table3_exp_comm(lenCompS_comm)
@@ -222,4 +250,74 @@ t = table_format(x = t3_samples[,-which(names(t3_samples)=="fleet")],
                  row.names = FALSE)
 
 kableExtra::save_kable(t, file = file.path(getwd(),"doc","length_samps_S.tex"))
+
+
+####################
+####North age samples
+###################
+
+t4_or_rec <- table3_noexp(ageCompN_OR_Rec)
+t4_wa_rec <- table3_noexp(ageCompN_WA_Rec)
+t4_lam <- table4_fromCAAL(ageCAAL_N_LamThesis) ###<- add from CAAL
+
+t4_wcgbts <- table3_exp(ageCompN_sex3_WCGBTS, region = "north", type = "age")
+t4_tri <- table3_exp(ageCompN_sex3_Triennial, region = "north", type = "age")
+
+t4_com <-table3_exp_comm(ageCompN_comm)
+
+colnames_ordered <- c("year","fleet","Fleet","Gender","Units","Nsamp","Ntows","Nfish","Nmale","Nfemale")
+t4_samples <- data.frame(rbind
+                         (t4_or_rec, t4_wa_rec, 
+                           t4_lam,
+                           t4_wcgbts, t4_tri,
+                           t4_com))[,colnames_ordered]
+t4_samples = t4_samples[order(t4_samples$fleet, t4_samples$year),]
+
+colnames <- c("Year","Fleet","Gender","Units","Nsamp","Ntows","Nfish","Nmale","Nfemale")
+
+t = table_format(x = t4_samples[,-which(names(t4_samples)=="fleet")],
+                 caption = 'Sample sizes of age composition data for the north model.',
+                 label = 'age_samps_N',
+                 longtable = TRUE,
+                 font_size = 9,
+                 digits = 2,
+                 landscape = FALSE,
+                 col_names = colnames,
+                 row.names = FALSE)
+
+kableExtra::save_kable(t, file = file.path(getwd(),"doc","age_samps_N.tex"))
+
+
+####################
+####South age samples
+###################
+
+t4_lam <- table4_fromCAAL(ageCAAL_S_LamThesis) ###<- add from CAAL
+t4_hkl <- table3_noexp(ageCompS_HKL)
+
+t4_wcgbts <- table3_exp(ageCompS_sex3_WCGBTS, region = "south", type = "age")
+t4_tri <- table3_exp(ageCompS_sex3_Triennial, region = "south", type = "age")
+
+t4_com <-table3_exp_comm(ageCompS_comm)
+
+colnames_ordered <- c("year","fleet","Fleet","Gender","Units","Nsamp","Ntows","Nfish","Nmale","Nfemale")
+t4_samples <- data.frame(rbind
+                         (t4_lam,
+                           t4_hkl, t4_wcgbts, t4_tri,
+                           t4_com))[,colnames_ordered]
+t4_samples = t4_samples[order(t4_samples$fleet, t4_samples$year),]
+
+colnames <- c("Year","Fleet","Gender","Units","Nsamp","Ntows","Nfish","Nmale","Nfemale")
+
+t = table_format(x = t4_samples[,-which(names(t4_samples)=="fleet")],
+                 caption = 'Sample sizes of age composition data for the south model.',
+                 label = 'age_samps_S',
+                 longtable = TRUE,
+                 font_size = 9,
+                 digits = 2,
+                 landscape = FALSE,
+                 col_names = colnames,
+                 row.names = FALSE)
+
+kableExtra::save_kable(t, file = file.path(getwd(),"doc","age_samps_S.tex"))
 
