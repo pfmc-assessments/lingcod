990b53a70daf26c8456b3eaf2b7afd9f97d6e91b
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2021-06-25 15:49:27 -0700

    mess of output vs model in table_outputconfig params vs parms

--- R/table_outputconfig.R
+++ R/table_outputconfig.R
@@ -1,11 +1,12 @@
-#' Summarize the configuration of the SS model
+#' Summarize the configuration of the SS output
 #'
 #' @param output A list from [r4ss::SS_output].
+#' @export
 #' @examples
-#' table_modelconfig(model)
-table_modelconfig <- function(output) {
+#' table_outputconfig(output)
+table_outputconfig <- function(output) {
 
-  slxtable <- table_modelslx(output)
+  slxtable <- table_outputslx(output)
 
   data <- data.frame(
     Section = c(
@@ -25,39 +26,39 @@ table_modelconfig <- function(output) {
       "Fishing mortality ($F$) method"
     ),
     Parameterization = c(
-      model[["accuage"]],
-      ifelse(model[["nsexes"]] == 2, "Females \\& males", "Females"),
+      output[["accuage"]],
+      ifelse(output[["nsexes"]] == 2, "Females \\& males", "Females"),
       sprintf(
         "%i-%i cm by %i cm bins",
-        model[["lbinspop"]][1],
-        dplyr::last(model[["lbinspop"]]),
-        model[["lbinspop"]][2] - model[["lbinspop"]][1]
+        output[["lbinspop"]][1],
+        dplyr::last(output[["lbinspop"]]),
+        output[["lbinspop"]][2] - output[["lbinspop"]][1]
       ),
-      sprintf("%i+", model[["summary_age"]]),
-      model[["nareas"]],
-      model[["nseasons"]],
-      model[["ngpatterns"]],
-      model[["startyr"]],
-      ifelse(all(model[["catch_units"]] == 1), "mt", "I don't know"),
+      sprintf("%i+", output[["summary_age"]]),
+      output[["nareas"]],
+      output[["nseasons"]],
+      output[["ngpatterns"]],
+      output[["startyr"]],
+      ifelse(all(output[["catch_units"]] == 1), "mt", "I don't know"),
       sprintf(
         "%i-%i cm by %i cm bins",
-        model[["lbins"]][1],
-        dplyr::last(model[["lbins"]]),
-        model[["lbins"]][2] - model[["lbins"]][1]
+        output[["lbins"]][1],
+        dplyr::last(output[["lbins"]]),
+        output[["lbins"]][2] - output[["lbins"]][1]
       ),
       sprintf(
         "%i-%i cm by %i year",
-        model[["agebins"]][1],
-        dplyr::last(model[["agebins"]]),
-        model[["agebins"]][2] - model[["agebins"]][1]
+        output[["agebins"]][1],
+        dplyr::last(output[["agebins"]]),
+        output[["agebins"]][2] - output[["agebins"]][1]
       ),
-      model[["endgrowth"]] %>% dplyr::filter(Sex == 1, Age_Beg < 5, Age_Mat != 0) %>% dplyr::select(Age_Beg,Age_Mat) %>% round(3) %>% dplyr::pull(Age_Beg) %>% dplyr::first(),
-      model[["recruitpars"]]%>%dplyr::filter(grepl("Main", type)) %>% dplyr::pull(Yr) %>% min,
-      ifelse(model[["F_method"]] == 3, "Hybrid $F$", "I don't know")
+      output[["endgrowth"]] %>% dplyr::filter(Sex == 1, Age_Beg < 5, Age_Mat != 0) %>% dplyr::select(Age_Beg,Age_Mat) %>% round(3) %>% dplyr::pull(Age_Beg) %>% dplyr::first(),
+      output[["recruitpars"]]%>%dplyr::filter(grepl("Main", type)) %>% dplyr::pull(Yr) %>% min,
+      ifelse(output[["F_method"]] == 3, "Hybrid $F$", "I don't know")
     )
   )
 
-  ## Format the model_specifications table
+  ## Format the output_specifications table
   kableExtra::kbl(rbind(
     data,
     slxtable %>%
@@ -68,8 +69,8 @@ table_modelconfig <- function(output) {
     ),
     booktabs = TRUE, escape = FALSE,
     format = "latex", longtable = TRUE,
-    caption = "Specifications and structure of the base model.",
-    label = "model-specifications"
+    caption = "Specifications and structure of the base output.",
+    label = "output-specifications"
   ) %>%
     kableExtra::pack_rows("Population characteristics", 1, 7) %>%
     kableExtra::pack_rows("Data characteristics", 8, 13) %>%
@@ -77,8 +78,8 @@ table_modelconfig <- function(output) {
 
 }
 
-table_modelslx <- function(output) {
-  slxtable <- model[["parameters"]] %>%
+table_outputslx <- function(output) {
+  slxtable <- output[["parameters"]] %>%
     dplyr::filter(grepl("Size", Label)) %>% dplyr::select(Label, Phase) %>%
     tidyr::separate(Label, remove = FALSE, extra = "merge", sep = "_", into = c("type", "form", "parameter", "something", "fleet")) %>%
     tidyr::separate(fleet, extra = "drop", sep = "\\(|\\)", into = c("fleet", "fleetnumber")) %>% 
