b6bf304641acdf9bc5d4a757e75764233fca15a4
Kelli.Johnson <1536491690113302@mil>
2021-06-25 10:39:16 -0700

    fix(add_figure_ling) file paths are a pain in rear
    trying to make it work with both relative and full paths,
    something about the csv makes it where it can have one or the other

--- R/add_figure_ling.R
+++ R/add_figure_ling.R
@@ -13,9 +13,25 @@
 #' add_figure_ling(csv=figurecsv, "data_plot")
 #' }
 add_figure_ling <- function(csv, grep = ".+", caption, alt_caption) {
+
   if ("loc" %in% colnames(csv) & !("filein" %in% colnames(csv))) {
-    csv[, "loc"] <- file.path(csv[["loc"]], paste0(csv[["label"]], ".png"))
+    csv[, "loc"] <- file.path(
+      R.utils::getRelativePath(
+        csv[["loc"]],
+        file.path(
+          dirname(system.file(package = "lingcod")),
+          "doc"
+        )
+      ),
+      paste0(csv[["label"]], ".png"))
+    if (!file.exists(csv[1,"loc"])) {
+      if (all(lapply(strsplit(csv[["loc"]], "/"), "[[", 1) == "models") &
+          basename(getwd()) == "doc") {
+        csv[["loc"]] <- file.path("..", csv[["loc"]])
+      }
+    }
   }
+
   info <- csv %>%
     dplyr::filter(grepl(grep, label)) %>%
     dplyr::rename_with(
