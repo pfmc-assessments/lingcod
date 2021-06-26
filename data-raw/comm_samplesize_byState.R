6aa7751ce962760ad84391d905a478f60efe2020
Brian Langseth <brian.langseth@noaa.gov>
2021-06-25 23:55:24 -0700

    create sample size tables by state
    *Only for commercial gear
    #89

--- data-raw/comm_samplesize_byState.R
+++ data-raw/comm_samplesize_byState.R
@@ -8,7 +8,7 @@
 
 ###################################################################################
 #Script to provide sample sizes (nfish) by state and gear. Used to generate table
-#of sample sizes (table_3.R)
+#of sample sizes (table_3.R). Only needed for north
 ###Lengths
 temp_dat.n = bds.pacfin.n[!is.na(bds.pacfin.n$lengthcm),]
 nlen_state.n <- aggregate(temp_dat.n$lengthcm,
@@ -19,46 +19,76 @@ nlen_state.n <- aggregate(temp_dat.n$lengthcm,
                           FUN = length)
 names(nlen_state.n)[ncol(nlen_state.n)] <- "nfish"
 write.csv(nlen_state.n, 
-          file.path(getwd(),"data-raw","pacfin_length_state_gear_sample_size_North.csv"), 
+          file.path(getwd(),"data-raw","pacfin_length_nfish_state_North.csv"), 
           row.names = FALSE)
 
-temp_dat.s = bds.pacfin.s[!is.na(bds.pacfin.s$lengthcm),]
-nlen_state.s <- aggregate(temp_dat.s$lengthcm,
-                          by = list(yr = temp_dat.s$SAMPLE_YEAR,
-                                    sex = temp_dat.s$SEX,
-                                    fleet = temp_dat.s$fleet,
-                                    state = temp_dat.s$state),
+# temp_dat.s = bds.pacfin.s[!is.na(bds.pacfin.s$lengthcm),]
+# nlen_state.s <- aggregate(temp_dat.s$lengthcm,
+#                           by = list(yr = temp_dat.s$SAMPLE_YEAR,
+#                                     sex = temp_dat.s$SEX,
+#                                     fleet = temp_dat.s$fleet,
+#                                     state = temp_dat.s$state),
+#                           FUN = length)
+# names(nlen_state.s)[ncol(nlen_state.s)] <- "nfish"
+# write.csv(nlen_state.s, 
+#           file.path(getwd(),"data-raw","pacfin_length_nfish_state_South.csv"), 
+#           row.names = FALSE)
+
+###Age
+temp_dat.n = bds.pacfin.n[!is.na(bds.pacfin.n$Age),]
+nage_state.n <- aggregate(temp_dat.n$Age,
+                          by = list(yr = temp_dat.n$SAMPLE_YEAR,
+                                    sex = temp_dat.n$SEX,
+                                    fleet = temp_dat.n$fleet,
+                                    state = temp_dat.n$state),
                           FUN = length)
-names(nlen_state.s)[ncol(nlen_state.s)] <- "nfish"
-write.csv(nlen_state.s, 
-          file.path(getwd(),"data-raw","pacfin_length_state_gear_sample_size_South.csv"), 
+names(nage_state.n)[ncol(nage_state.n)] <- "nfish"
+write.csv(nage_state.n, 
+          file.path(getwd(),"data-raw","pacfin_age_nfish_state_North.csv"), 
           row.names = FALSE)
 
-################################
-#Get ntrip by state. 
+# temp_dat.s = bds.pacfin.s[!is.na(bds.pacfin.s$Age),]
+# nage_state.s <- aggregate(temp_dat.s$Age,
+#                           by = list(yr = temp_dat.s$SAMPLE_YEAR,
+#                                     sex = temp_dat.s$SEX,
+#                                     fleet = temp_dat.s$fleet,
+#                                     state = temp_dat.s$state),
+#                           FUN = length)
+# names(nage_state.s)[ncol(nage_state.s)] <- "nfish"
+# write.csv(nage_state.s, 
+#           file.path(getwd(),"data-raw","pacfin_age_nfish_state_South.csv"), 
+#           row.names = FALSE)
+
+
+
+
+##Script to provide sample sizes (ntows) by state and gear. Used to generate table
+#of sample sizes (table_3.R)
+
+#Get ntows by state. 
 #Issue 1: Doing by SEX did not work. It does not produce comparable 
-#results to our comps in lenN_comm, so treat as all together. 
-#Issue 2: Need to keep NA lenghts in. Results across states are comparable
+#results to our comps in lenN_comm, so treat as all sexes together. 
+#Issue 2: Need to keep NA lengths in. Results across states are comparable
 #to standard approach using unsexed comps without state distinction. 
 
-#North
+#North Lengths
 temp <- bds.pacfin.n[!is.na(bds.pacfin.n$lengthcm), ]
 temp <- bds.pacfin.n 
 
 fish <- aggregate(SEX ~ fishyr + fleet + state, temp,
-                 FUN = function(x) { length(x) } )
+                  FUN = function(x) { length(x) } )
 
 trips <- aggregate(SAMPLE_NO~fishyr + fleet + state, temp,
-                  FUN = function(x) { length(unique(x)) } )
-names(trips) <- c("yr","fleet","state","ntows")
+                   FUN = function(x) { length(unique(x)) } )
 
+# #Getting 12 tows in 1987 for FG. Below I get 14 (if keep NA lengthcm then this resolves)
+# aggregate(SAMPLE_NO ~ fishyr + fleet, trips, FUN = sum)
+
+names(trips) <- c("yr","fleet","state","ntows")
 write.csv(trips, 
           file.path(getwd(),"data-raw","pacfin_length_ntows_state_North.csv"), 
           row.names = FALSE)
-
 # #Testing to ensure results over all states are comparable
-# #Getting 12 tows in 1987 for FG. Below I get 14 (if keep NA lengthcm then this resolves)
-# aggregate(SAMPLE_NO ~ fishyr + fleet, trips, FUN = sum)
 # 
 # bds.pacfin.n.exp$SEX = "U"
 # comps.n <- PacFIN.Utilities::getComps(Pdata = bds.pacfin.n.exp,
@@ -72,26 +102,70 @@ write.csv(trips,
 #                                               digits = 3,
 #                                               dummybins = FALSE)
 
+#North ages
+#Need to keep NA age in. In this case results across states are comparable
+#to standard approach using unsexed comps without state distinction
+#provided years without valid ages are removed after the fact. 
+#This results in the same number of tows as for length
 
-#For south
-temp <- bds.pacfin.s[!is.na(bds.pacfin.s$lengthcm), ]
-temp <- bds.pacfin.s
+temp <- bds.pacfin.n[!is.na(bds.pacfin.n$Age), ]
+temp <- bds.pacfin.n 
 
 fish <- aggregate(SEX ~ fishyr + fleet + state, temp,
-                 FUN = function(x) { length(x) } )
+                  FUN = function(x) { length(x) } )
 
 trips <- aggregate(SAMPLE_NO~fishyr + fleet + state, temp,
-                  FUN = function(x) { length(unique(x)) } )
+                   FUN = function(x) { length(unique(x)) } )
 
-#Years added where the following shows only NAs for a fleet (1979,1980 for FG). These
-#must be removed.
-table(bds.pacfin.s$fishyr,is.na(bds.pacfin.s$lengthcm),bds.pacfin.s$fleet)
+aggregate(SAMPLE_NO ~ fishyr + fleet, trips, FUN = sum)
 
-trips2 <- trips[!(trips$fishyr %in% c(1979,1980) & trips$fleet=="FG"),]
+noages = table(temp$fishyr,is.na(temp$Age),temp$fleet)
+noages_yrFG <- names(which(noages[,"FALSE","FG"]==0))
+noages_yrTW <- names(which(noages[,"FALSE","TW"]==0))
 
-write.csv(trips2, 
-          file.path(getwd(),"data-raw","pacfin_length_ntows_state_South.csv"), 
+trips_rec <- rbind(trips[which(trips$fleet == "FG" & !trips$fishyr %in% noages_yrFG),],
+                   trips[trips$fleet == "TW" & !trips$fishyr %in% noages_yrTW,])
+
+names(trips) <- c("yr","fleet","state","ntows")
+write.csv(trips_rec, 
+          file.path(getwd(),"data-raw","pacfin_age_ntows_state_North.csv"), 
           row.names = FALSE)
+
+# #Testing to ensure results over all states are comparable
+# bds.pacfin.n.exp$SEX = "U"
+# comps.n <- PacFIN.Utilities::getComps(Pdata = bds.pacfin.n.exp,
+#                                       Comps = "AGE")
+# 
+# tempAgeN <- PacFIN.Utilities::writeComps(inComps = comps.n,
+#                                               fname = "data/ageCompN_comm_all_unesexed.csv",
+#                                               lbins = info_bins$age,
+#                                               sum1 = TRUE,
+#                                               partition = 2,
+#                                               digits = 3,
+#                                               dummybins = FALSE)
+
+
+# #South Lengths
+# #Not needed since all one state (CA) but tested anyway
+#
+# temp <- bds.pacfin.s[!is.na(bds.pacfin.s$lengthcm), ]
+# temp <- bds.pacfin.s
+# 
+# fish <- aggregate(SEX ~ fishyr + fleet + state, temp,
+#                  FUN = function(x) { length(x) } )
+# 
+# trips <- aggregate(SAMPLE_NO~fishyr + fleet + state, temp,
+#                   FUN = function(x) { length(unique(x)) } )
+# 
+# #Years added where the following shows only NAs for a fleet (1979,1980 for FG). These
+# #must be removed.
+# table(bds.pacfin.s$fishyr,is.na(bds.pacfin.s$lengthcm),bds.pacfin.s$fleet)
+# 
+# trips2 <- trips[!(trips$fishyr %in% c(1979,1980) & trips$fleet=="FG"),]
+# 
+# write.csv(trips2, 
+#           file.path(getwd(),"data-raw","pacfin_length_ntows_state_South.csv"), 
+#           row.names = FALSE)
 
 # #Testing to ensure results over all states are comparable
 # aggregate(SAMPLE_NO ~ fishyr + fleet, trips, FUN = sum)
@@ -107,31 +181,4 @@ write.csv(trips2,
 #                                               partition = 2,
 #                                               digits = 3,
 #                                               dummybins = FALSE)
-###################################
 
-
-###Age
-temp_dat.n = bds.pacfin.n[!is.na(bds.pacfin.n$Age),]
-nage_state.n <- aggregate(temp_dat.n$Age,
-                          by = list(yr = temp_dat.n$SAMPLE_YEAR,
-                                    sex = temp_dat.n$SEX,
-                                    fleet = temp_dat.n$fleet,
-                                    state = temp_dat.n$state),
-                          FUN = length)
-names(nage_state.n)[ncol(nage_state.n)] <- "nfish"
-write.csv(nage_state.n, 
-          file.path(getwd(),"data-raw","pacfin_age_state_gear_sample_size_North.csv"), 
-          row.names = FALSE)
-
-temp_dat.s = bds.pacfin.s[!is.na(bds.pacfin.s$Age),]
-nage_state.s <- aggregate(temp_dat.s$Age,
-                          by = list(yr = temp_dat.s$SAMPLE_YEAR,
-                                    sex = temp_dat.s$SEX,
-                                    fleet = temp_dat.s$fleet,
-                                    state = temp_dat.s$state),
-                          FUN = length)
-names(nage_state.s)[ncol(nage_state.s)] <- "nfish"
-write.csv(nage_state.s, 
-          file.path(getwd(),"data-raw","pacfin_age_state_gear_sample_size_South.csv"), 
-          row.names = FALSE)
-####################################################################################
---
+++ data-raw/table_sampeSize_comm_byState.R
@@ -0,0 +1,109 @@
+#Script to create table of samples sizes of length and age comps by state
+#for commercial data. Adopted from table_sampleSize.R
+#Functions are not pretty and brute force. Multiple are needed given slightly different
+#formats for all of the data. This could be cleaned in future. 
+
+###########################Functions for various data types#################################
+
+#Based off of table3 and 4 from previous (2017) stock assessment
+
+#Function to pull commercial samples sizes by state
+#Type is "length" or "age"
+table3_commState = function(type){
+  
+  #Read in nfish
+  samps_raw_nfish <- read.csv(file.path(getwd(), "data-raw", 
+                                        paste0("pacfin_", type, "_nfish_state_North.csv")),
+                              header=T)
+  #combine all sex as one because ntows is calculated over all genders
+  samps_raw_nfish[samps_raw_nfish$sex %in% c("F","M"),"Sex"] = 0 
+  samps_raw_nfish[samps_raw_nfish$sex %in% c("U"),"Sex"] = 0
+  
+  samps = aggregate(samps_raw_nfish$nfish, 
+                    by = list("year" = samps_raw_nfish$yr, 
+                              "fleet" = samps_raw_nfish$fleet, 
+                              "sex" = samps_raw_nfish$Sex, 
+                              "state" = samps_raw_nfish$state), 
+                    FUN = sum)
+  names(samps)[ncol(samps)] = "Nfish"
+  
+  samps$fleet_name = samps$fleet
+  samps[samps$fleet == "FG","fleet"] = 2
+  samps[samps$fleet == "TW","fleet"] = 1
+  samps$Fleet = NULL
+  samps$Fleet = paste(get_fleet(as.numeric(samps$fleet))$label_short, samps$state)
+  
+  #Read in tows
+  ntows <- read.csv(file.path(getwd(), "data-raw", 
+                                        paste0("pacfin_", type, "_ntows_state_North.csv")),
+                              header=T)
+
+  if(type == "length"){
+  out <- merge(samps, ntows, by.x = c("year", "fleet_name", "state"),
+               by.y = c("yr", "fleet", "state"))
+  }
+  if(type == "age"){
+    out <- merge(samps, ntows, by.x = c("year", "fleet_name", "state"),
+                 by.y = c("fishyr", "fleet", "state"))
+    names(out)[ncol(out)] <- "ntows"
+  }
+
+  return(out[,c("year","fleet","Fleet","ntows","Nfish")])
+}
+
+##################################End of functions##################################
+
+####################
+####North length samples
+###################
+
+t3_comState <- table3_commState(type = "length")
+
+t3_comState = t3_comState[order(t3_comState$fleet, t3_comState$Fleet, t3_comState$year),]
+
+#Can output csv's to test
+#write.csv(t3_comState, file.path(getwd(), "data-raw", "t3_north_comm_length_byState.csv"))
+
+aggregate(Nfish~year + fleet, t3_comState, FUN=sum)
+
+colnames <- c("Year","Fleet","Ntows","Nfish")
+
+t = table_format(x = t3_comState[,-which(names(t3_comState) %in% c("fleet"))],
+                 caption = 'Sample sizes of commercial length composition data by state for the north model
+                 combined across sexes.',
+                 label = 'sample-size-length-byState',
+                 longtable = TRUE,
+                 font_size = 9,
+                 digits = 2,
+                 landscape = FALSE,
+                 col_names = colnames,
+                 row.names = FALSE)
+
+kableExtra::save_kable(t, file = file.path(getwd(),"doc","length_samps_comm_byState_North.tex"))
+
+
+####################
+####North age samples
+###################
+
+t4_comState <- table3_commState(type = "age")
+
+t4_comState = t4_comState[order(t4_comState$fleet, t4_comState$Fleet, t4_comState$year),]
+
+#Can output csv's to test
+#write.csv(t4_comState, file.path(getwd(), "data-raw", "t4_north_comm_age_byState.csv"))
+
+colnames <- c("Year","Fleet","Ntows","Nfish")
+
+t = table_format(x = t4_comState[,-which(names(t4_comState) %in% c("fleet"))],
+                 caption = 'Sample sizes of commercial age composition data by state for the north model
+                 combined across sexes.',
+                 label = 'sample-size-age-byState',
+                 longtable = TRUE,
+                 font_size = 9,
+                 digits = 2,
+                 landscape = FALSE,
+                 col_names = colnames,
+                 row.names = FALSE)
+
+kableExtra::save_kable(t, file = file.path(getwd(),"doc","age_samps_comm_byState_North.tex"))
