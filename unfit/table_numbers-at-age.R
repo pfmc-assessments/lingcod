09728a7698386b5ba8fd5d962b42f6609dabf02c
Ian Taylor <ian.taylor@noaa.gov>
2021-06-26 22:05:57 -0700

    add numbers-at-age tables to web #95

---
+++ unfit/table_numbers-at-age.R
@@ -0,0 +1,23 @@
+# run this for a given model
+if (FALSE) {
+  r4ss::SSexecutivesummary(mod.2021.n.020.001)
+  r4ss::SSexecutivesummary(mod.2021.s.014.001)
+}
+
+for (Area in c("North", "South")) {
+  for (sex in c("f", "m")) {
+    sep <- ifelse(sex == "f", ".", "_")
+    file.path("models",
+              info_basemodels[[Area]],
+              "tables",
+              paste0("natage", sep, sex, ".csv")) %>%
+      read.csv() %>%
+      round(1) %>%
+      kableExtra::kbl(format = "html") %>%
+      kableExtra::kable_styling("striped") %>%
+      kableExtra::save_kable(file.path(
+                    "docs",
+                    paste0(Area, "_natage.",
+                           sex, ".html")))
+  }
+}
