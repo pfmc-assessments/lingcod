11007138286922be597ffd8e5bca178bdafef140
Ian Taylor <ian.taylor@noaa.gov>
2021-08-12 16:52:06 -0700

    add column to projections table
    - revise table_projections()
    - add additional model sens = 629 with 40:10 rule turned off

--- R/table_projections.R
+++ R/table_projections.R
@@ -12,11 +12,13 @@ table_projections <-
   function(output,
            caption = paste("Projections of potential overfishing limits (OFLs; mt),",
                            "allowable biological catches (ABCs; mt),",
-                           "estimated summary biomass (mt), spawning biomass (mt), and fraction unfished",
-                           "based on assumed removals for the most recent two years.",
-                           "Assumed removals for 2021 and 2022 are average",
-                           "catch from the period 2016-2020 with a 40:60",
-                           "split of trawl:fixed-gear."),
+                           "annual catch limits (ACLs; mt),",
+                           "estimated summary biomass (mt), spawning biomass (mt), and fraction unfished.",
+                           "Values are based on removals for the first two years.",
+                           "ABCs include a buffer for scientific uncertainty based on",
+                           "a Pstar of 0.45 and the category 2 default sigma = 1.0.",
+                           "ACLs additionally include the 40:10 adjustment for projections",
+                           "which fall below the B40 reference point."),
            label = "table-projections-base"
            )
 {
@@ -48,7 +50,31 @@ table_projections <-
   }
   # make the table
   tab <- read.csv(tab_csv, check.names = FALSE)
+
   # I'm sure all the following could be done in dplyr in 1/10th the space
+
+  # get forecast for model with 40:10 adjustment turned off
+  forecast_dir2 <- output$inputs$dir %>%
+    stringr::str_sub(end = nchar("models/2021.n.001.")) %>%
+    paste0("629_forecast_no4010")
+  if (!dir.exists(forecast_dir2)) {
+    if (dir.exists(file.path("..", forecast_dir2))) {
+      forecast_dir2 <- file.path("..", forecast_dir2)
+    } 
+  }
+  quants <- r4ss::SS_output(
+                      forecast_dir2,
+                      printstats = FALSE,
+                      verbose = FALSE,
+                      covar = FALSE
+                    )[["derived_quants"]]
+  ABC_vals <- round(quants[paste0("ForeCatch_", 2021:2032), "Value"], 2)
+  # terrible approach to repeating column 3
+  tab <- tab[,c(1:3, 3, 4:ncol(tab))]
+  # rename old ABC to ACL
+  names(tab)[4] <- "ACL Catch (mt)"
+  # add new ABC without 40:10 based on input from DeVore
+  tab[,"ABC Catch (mt)"] <- ABC_vals
 
   # fixed catch values to a new column
   assumed <- formatC(
@@ -58,7 +84,9 @@ table_projections <-
     tab[,"ABC Catch (mt)"]
     )
   assumed[tab$Year > output$endyr + 2] <- "-"
-  tab[tab$Year <= output$endyr + 2, c("Predicted OFL (mt)", "ABC Catch (mt)")] <- "-"
+  tab[tab$Year <= output$endyr + 2, c("Predicted OFL (mt)",
+                                      "ABC Catch (mt)",
+                                      "ACL Catch (mt)")] <- "-"
   tab <- cbind(tab$Year, assumed, tab[,-1])
   names(tab)[names(tab) == "assumed"] <- "Assumed Removal (mt)"
   colnames(tab)[1] <- "Year"
