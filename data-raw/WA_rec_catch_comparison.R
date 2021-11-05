26a3d1f82b8fe54ba63baef7fb8f043887964b49
Ian Taylor <ian.taylor@noaa.gov>
2021-11-05 13:22:45 -0700

    add additional catch figures (r4ss plots + 1 extra WA rec #30)

--- data-raw/WA_rec_catch_comparison.R
+++ data-raw/WA_rec_catch_comparison.R
@@ -1,22 +1,25 @@
 # plot Rec WA catch in biomass and numbers
-png(file.path("figures", "WA_rec_catch_comparison.png"),
+png(file.path("figures", "WA_rec_catch_comparison_after_fix.png"),
               res = 300, width = 6.5, height = 6.5, pointsize = 10, units = "in")
-par(mfrow = c(2, 1), mar = c(2, 4, 2, 1))
+par(mfrow = c(2, 1), mar = c(2, 4, 1, 1))
 
 # load north models from 2017 and pre-STAR 2021
 if (!exists("mod.2017.n.001.001")) {
   get_mod('n', 1, yr = 2017)
-  get_mod('n', 22)
+  get_mod('n', 23)
 }
 
 # catch in biomass
-mod.2021.n.022.001$timeseries %>%
+mod.2021.n.023.001$timeseries %>%
   dplyr::filter(Yr %in% 1960:2017) %>%
   dplyr::select(Yr, "dead(B):_3") %>%
   plot(
     type = "l", lwd = 2, xlim = c(1960, 2017),
     ylab = "Catch in biomass (mt)",
-    main = "Washington recreational catch"
+    #main = "Washington recreational catch",
+    ylim = c(0,200),
+    yaxs = "i",
+    las = 1
   )
 mod.2017.n.001.001$timeseries %>%
   dplyr::filter(Yr %in% 1960:2017) %>%
@@ -29,12 +32,15 @@ legend("topleft",
 )
 
 # catch in numbers
-mod.2021.n.022.001$timeseries %>%
+mod.2021.n.023.001$timeseries %>%
   dplyr::filter(Yr %in% 1960:2017) %>%
   dplyr::select(Yr, "dead(N):_3") %>%
   plot(
     type = "l", lwd = 2, xlim = c(1960, 2017),
-    ylab = "Catch in numbers (1000s)"
+    ylab = "Catch in numbers (1000s)",
+    ylim = c(0,60),
+    yaxs = "i",
+    las = 1
   )
 mod.2017.n.001.001$timeseries %>%
   dplyr::filter(Yr %in% 1960:2017) %>%
