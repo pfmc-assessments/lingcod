a7dacc31eb06e83e06d047c4cb1d5018611045f0
Margaret.Siple <1595080254113302@mil>
2026-09-17 14:33:55 -0700

    add start of script for making asar template

---
+++ unfit/make_asar_report_script.R
@@ -0,0 +1,3 @@
+# asar setup (it is recommended in the asar tutorial that we save this script somewhere, I picked unfit/ based on the README instructions)
+
+output_file <- here::here("data","Report.sso")
