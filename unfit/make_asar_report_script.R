f20bc6bd5082e09d8a5df70d0e5bc4611f49e3b8
Margaret Siple <margaret.siple@noaa.gov>
2026-09-17 16:09:59 -0700

    udpate north script w create_template(), list of authors, etc. #204

--- unfit/make_asar_report_script.R
+++ unfit/make_asar_report_script.R
@@ -2,7 +2,6 @@
 library(stockplotr)
 library(asar)
 
-
 # There is one Report.sso file each for the final model for North and South
 n_output_file <- here::here("models","2021.n.023.001_fixWAreccatchhistory","Report.sso")
 s_output_file <- here::here("models","2021.s.018.001_fixTri3","Report.sso")
@@ -12,8 +11,32 @@ lingcod_conout <- stockplotr::convert_output(
   file = n_output_file,
   fleet_names = c("TW","FG"), # got these from doc/catch-comm-fleetstructure.Rmd
   model = "SS3", # optional, function recognizes model
-  save_dir = here::here("data","lingcod_output.rda")
+  save_dir = here::here("doc", "report_north", "lingcod_output.rda")
 )
 
+# Read in converted output file
+load(here::here("doc", "report_north", "lingcod_output.rda"))
 
+# Run function
+  # The two functions below use the current wd to save plots, so it is set here. 
+  # I don't love that but it works.
+setwd(here::here("doc","report_north"))
+stockplotr::save_all_plots(dat = out_new) 
 
+# Create asar template
+# Note: 
+asar::create_template(
+  format = "pdf",
+  office = "NWFSC",
+  region = "U.S. West Coast",
+  species = "Lingcod",
+  spp_latin = "Ophiodon elongatus",
+  year = 2027,
+  author = c("Brian Langseth"="NWFSC", 
+  "Vladlena Gertseva"="NWFSC", 
+  "Margaret Siple"="NWFSC", 
+  "Megan Feddern"="NWFSC"),
+  param_names = c("TW","FG"),
+  param_values = c("Trawl", "Fixed gear"),
+  model_results = "../doc/report_north/lingcod_output.rda" # converted model output
+)
